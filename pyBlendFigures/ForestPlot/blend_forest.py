5cafc1a3ca3dcd83909d4f682273c6dff7a42ee0
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-03-21 18:43:35 +0000

    Set material

--- pyBlendFigures/ForestPlot/blend_forest.py
+++ pyBlendFigures/ForestPlot/blend_forest.py
@@ -5,7 +5,7 @@ import statistics
 
 
 class ForestLine:
-    def __init__(self, row_values, radius_of_coefficient, rounding):
+    def __init__(self, row_values, radius_of_coefficient, rounding, object_colour):
 
         # Set the values from the row that was read in via the temp csv
         self.var_name, self.coef, self.coef_plot, self.lb, self.lb_plot, self.ub, self.ub_plot = row_values
@@ -13,6 +13,7 @@ class ForestLine:
         # Set the values required to create the blend objects
         self._radius = radius_of_coefficient
         self._round = rounding
+        self._colour = object_colour
 
     def make_line(self, height_total, height_iter, scale=0.2):
         """
@@ -33,6 +34,7 @@ class ForestLine:
         # Make the block
         mesh = bpy.data.meshes.new(self.var_name)  # add the new mesh
         obj = bpy.data.objects.new(mesh.name, mesh)
+        create_emission_node(obj, self._colour)
         col = bpy.data.collections.get("Collection")
         col.objects.link(obj)
         bpy.context.view_layer.objects.active = obj
@@ -71,11 +73,15 @@ class ForestLine:
         # Create the primitive circle
         bpy.ops.mesh.primitive_circle_add(
             enter_editmode=True, align='WORLD', location=(float(self.coef_plot), y_mean_val, 0.1), radius=self._radius)
-        bpy.context.object.name = f"{object_name}_coef"
 
-        # Circle to default to edit mode, use this to fill the circle in, then toggle out and deselect.
+        # Circle to default to edit mode, use this to fill the circle in.
         bpy.ops.mesh.edge_face_add()
         bpy.ops.object.editmode_toggle()
+
+        # Set the name and material of the circle, then toggle out and deselect.
+        bpy.context.object.name = f"{object_name}_var_name"
+        obj = bpy.context.object
+        create_emission_node(obj, self._colour)
         bpy.ops.object.select_all(action='DESELECT')
 
         # Return the y mean for positioning the text
@@ -96,10 +102,13 @@ def isolate_rows(path_to_csv):
         return [r for i, r in enumerate(csv.reader(csv_file)) if i > 0]
 
 
-def make_text(bound, y_mean_val, text, height_iter):
+def make_text(object_name, bound, y_mean_val, text, height_iter, object_colour):
     """
     Creates a text object starting at x 'bound' and at y 'y_mean_val' displaying 'text'. It is scaled to be equal to the
     objects via the 'height_iter'
+
+    :param object_name: The name of the object
+    :type object_name: str
 
     :param bound: Starting x position
     :type bound: float
@@ -113,6 +122,9 @@ def make_text(bound, y_mean_val, text, height_iter):
     :param height_iter: Scaling about to make text relative to all other components
     :type height_iter: float
 
+    :param object_colour: RGBA colour of the object
+    :type object_colour: (int, int, int, int)
+
     :return: Nothing, make the text object then stop
     """
 
@@ -120,8 +132,10 @@ def make_text(bound, y_mean_val, text, height_iter):
     bpy.ops.object.text_add(location=(bound, y_mean_val, 0))
 
     # Set its name
-    ob = bpy.context.object
-    ob.data.body = text
+    obj = bpy.context.object
+    obj.data.body = text
+    create_emission_node(obj, object_colour)
+    bpy.context.object.name = f"{object_name}_var_name"
 
     # Scale it relative to all other elements
     bpy.ops.transform.resize(value=(height_iter, height_iter, height_iter))
@@ -155,6 +169,58 @@ def set_values(value, rounding_value):
         return set_rounding + "".join(["0" for _ in range(rounding_value - len(set_rounding))])
 
 
+def create_emission_node(obj, colour, strength=1.0):
+    """
+    Create an emission node for this object
+
+    Source: https://blender.stackexchange.com/questions/23436/control-cycles-material-nodes-and-material-
+            properties-in-python
+
+
+    :param obj: The current blender object
+
+    :param colour: RGBA colour
+    :type colour: (int, int, int, int)
+
+    :param strength: Intensity of the emission node
+    :type strength: float
+
+    :return: Nothing, make the emission node then stop
+    :rtype: None
+    """
+
+    # Test if material exists, If it does not exist, create it:
+    mat_name = obj.name
+    mat = (bpy.data.materials.get(mat_name) or
+           bpy.data.materials.new(mat_name))
+
+    # Enable 'Use nodes':
+    mat.use_nodes = True
+
+    # clear all nodes to start clean
+    nodes = mat.node_tree.nodes
+    nodes.clear()
+
+    # create emission node
+    node_emission = nodes.new(type='ShaderNodeEmission')
+    node_emission.inputs[0].default_value = colour
+    node_emission.inputs[1].default_value = strength
+    node_emission.location = 0, 0
+
+    # create output node
+    node_output = nodes.new(type='ShaderNodeOutputMaterial')
+    node_output.location = 400, 0
+
+    node = nodes.new('ShaderNodeBsdfDiffuse')
+    node.location = (100, 100)
+
+    # link nodes
+    links = mat.node_tree.links
+    links.new(node_emission.outputs[0], node_output.inputs[0])
+
+    obj.data.materials.append(mat)
+
+
 if __name__ == '__main__':
 
     csv_path = r"C:\Users\Samuel\PycharmProjects\pyBlendFigures\Tests\ScarletAverage1_TEMP.csv"
@@ -167,26 +233,31 @@ if __name__ == '__main__':
     var_bound = -0.8  # Defaults to 1, Expose
     numeric_bound = 0.8  # Defaults to 1, expose
     rounder = 3  # Defaults to 3, Expose
+    text_colour = (0, 0, 0, 0)
 
     # For each row represents a line we wish to plot
+    variable_names = []
     for row in csv_rows:
 
         # Create an object to construct the necessary components
-        forest_obj = ForestLine(row, coefficient_radius, rounder)
+        forest_obj = ForestLine(row, coefficient_radius, rounder, text_colour)
+        variable_names.append(forest_obj.var_name)
 
         # Create the line
         current_name = forest_obj.make_line(height_max, height_iterator)
         y_mean = forest_obj.make_coefficient(current_name)
 
         # Set the variable name
-        make_text(var_bound, y_mean, forest_obj.var_name, height_iterator)
+        make_text(forest_obj.var_name, var_bound, y_mean, forest_obj.var_name, height_iterator, text_colour)
 
         # Create the numeric string
         numeric = f"{set_values(forest_obj.coef, rounder)} ({set_values(forest_obj.ub, rounder)}; " \
                   f"{set_values(forest_obj.lb, rounder)})"
-        make_text(numeric_bound, y_mean, numeric, height_iterator)
+        make_text(forest_obj.var_name, numeric_bound, y_mean, numeric, height_iterator, text_colour)
 
         height_max -= height_iterator
+
+
 
     # todo set dimensions
     # todo create a base forest blend
