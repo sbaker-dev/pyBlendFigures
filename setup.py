9b087d4e994e253b7ebfe54c7387fe78520257a3
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-06-08 14:39:20 +0100

    Fix broken link in MANIFEST.in

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ MAINTAINER = 'Samuel Baker'
 MAINTAINER_EMAIL = 'samuelbaker.researcher@gmail.com'
 LICENSE = 'MIT'
 DOWNLOAD_URL = "https://github.com/sbaker-dev/pyBlendFigures"
-VERSION = "0.04.0"
+VERSION = "0.04.1"
 PYTHON_REQUIRES = ">=3.7"
 
 INSTALL_REQUIRES = [
