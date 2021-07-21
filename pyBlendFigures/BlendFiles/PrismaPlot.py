81bbef47615cd075a804732d3fbc4cc6636f5348
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-07-21 13:38:35 +0100

    Basic refactor

--- pyBlendFigures/BlendFiles/PrismaPlot.py
+++ pyBlendFigures/BlendFiles/PrismaPlot.py
@@ -34,48 +34,9 @@ class PrismaPlot:
                     position = self.positions[name]
                     print(name)
 
-                    # Set the width, which is the x position, relative to other columns of this row
-                    width = sum([self.widths[str(i - 1)] for i in range(col_i + 1)])
-                    if col_i != 0:
-                        width += self.spacing
-
-                    # Create the text object and set its origin to geometry
-                    obj = make_text(name, width, previous_height, position["Text"], 1, (255, 255, 255, 255),
-                                    align="CENTER")
-                    obj.select_set(True)
-                    bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
-
-                    # Isolate the texts center from location and width and height from dimensions
-                    x, y, _ = obj.location / 2
-                    x_d, y_d, _ = obj.dimensions / 2
-                    obj.select_set(False)
-
-                    # Create the bounding box
-                    vert_list = [((x - x_d) - self.line_width, (y + y_d) + self.line_width, -0.1),
-                                 ((x - x_d) - self.line_width, (y - y_d) - self.line_width, -0.1),
-                                 ((x + x_d) + self.line_width, (y - y_d) - self.line_width, -0.1),
-                                 ((x + x_d) + self.line_width, (y + y_d) + self.line_width, -0.1)]
-                    box_obj, mesh = make_mesh(name, (255, 255, 255, 255))
-                    mesh.from_pydata(vert_list, [], [[0, 1, 2, 3]])
-
-                    # Set the boxes origin to geometry
-                    box_obj.select_set(True)
-                    bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
-                    box_obj.location = obj.location
-
-                    # Set and then remove interior face
-                    bpy.ops.object.editmode_toggle()
-                    bpy.ops.mesh.inset(thickness=self.padding * self.line_width, depth=0)
-                    bpy.ops.mesh.delete(type='FACE')
-                    bpy.ops.object.editmode_toggle()
-
-                    # Set the profile of the object via a bevel
-                    bpy.ops.object.modifier_add(type='BEVEL')
-                    bpy.context.object.modifiers["Bevel"].segments = self.segments
-                    bpy.context.object.modifiers["Bevel"].profile = self.profile
-                    box_obj.select_set(False)
-
+                    self.make_text_box(name, col_i, position, previous_height)
                     previous_height -= self.dimensions[name][1]
+
                 except KeyError:
                     pass
 
@@ -100,6 +61,51 @@ class PrismaPlot:
 
         collection_cleanup("Collection")
         return widths, dimension_dict
+
+    def make_text_box(self, name, col_i, position, previous_height):
+        # Set the width, which is the x position, relative to other columns of this row
+        width = sum([self.widths[str(i - 1)] for i in range(col_i + 1)])
+        if col_i != 0:
+            width += self.spacing
+
+        # Create the text object and set its origin to geometry
+        obj = make_text(name, width, previous_height, position["Text"], 1, (255, 255, 255, 255),
+                        align="CENTER")
+        obj.select_set(True)
+        bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
+
+        # Isolate the texts center from location and width and height from dimensions
+        x, y, _ = obj.location / 2
+        x_d, y_d, _ = obj.dimensions / 2
+        obj.select_set(False)
+
+        self.make_bounding_box(x, y, x_d, y_d, name, obj)
+
+    def make_bounding_box(self, x, y, x_d, y_d, name, obj):
+        # Create the bounding box
+        vert_list = [((x - x_d) - self.line_width, (y + y_d) + self.line_width, -0.1),
+                     ((x - x_d) - self.line_width, (y - y_d) - self.line_width, -0.1),
+                     ((x + x_d) + self.line_width, (y - y_d) - self.line_width, -0.1),
+                     ((x + x_d) + self.line_width, (y + y_d) + self.line_width, -0.1)]
+        box_obj, mesh = make_mesh(name, (255, 255, 255, 255))
+        mesh.from_pydata(vert_list, [], [[0, 1, 2, 3]])
+
+        # Set the boxes origin to geometry
+        box_obj.select_set(True)
+        bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
+        box_obj.location = obj.location
+
+        # Set and then remove interior face
+        bpy.ops.object.editmode_toggle()
+        bpy.ops.mesh.inset(thickness=self.padding * self.line_width, depth=0)
+        bpy.ops.mesh.delete(type='FACE')
+        bpy.ops.object.editmode_toggle()
+
+        # Set the profile of the object via a bevel
+        bpy.ops.object.modifier_add(type='BEVEL')
+        bpy.context.object.modifiers["Bevel"].segments = self.segments
+        bpy.context.object.modifiers["Bevel"].profile = self.profile
+        box_obj.select_set(False)
 
 
 PrismaPlot()
