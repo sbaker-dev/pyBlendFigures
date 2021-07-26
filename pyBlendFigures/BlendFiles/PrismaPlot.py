63e3838559e6d7a2cd35e3b36cb1a959850031bf
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-07-26 11:01:26 +0100

    Make horizontal links

--- pyBlendFigures/BlendFiles/PrismaPlot.py
+++ pyBlendFigures/BlendFiles/PrismaPlot.py
@@ -26,26 +26,12 @@ class PrismaPlot:
         self.widths, self.dimensions = self._set_dimensions()
         self.box_names = []
 
-        previous_height = 0
-        for row_i in range(self.row_count):
-            for col_i in range(self.col_count):
-                try:
+        # Create the text and text boxes
+        self._create_text_boxes()
 
-                    # Isolate the name and position of this text object
-                    name = f"{col_i}-{row_i}"
-                    position = self.positions[name]
-                    print(name)
-
-                    self.make_text_box(name, col_i, position, previous_height)
-                    previous_height -= self.dimensions[name][1]
-
-                except KeyError:
-                    pass
-
-        # Create the center line
+        # Create the center line and the links to the center line from the sides
         self._create_center_line()
-
-        join_lines = [box for box in self.box_names if box.split("-")[0] == "1"]
+        self._create_links()
 
     def _set_dimensions(self):
 
@@ -69,7 +55,24 @@ class PrismaPlot:
         collection_cleanup("Collection")
         return widths, dimension_dict
 
-    def make_text_box(self, name, col_i, position, previous_height):
+    def _create_text_boxes(self):
+        previous_height = 0
+        for row_i in range(self.row_count):
+            for col_i in range(self.col_count):
+                try:
+
+                    # Isolate the name and position of this text object
+                    name = f"{col_i}-{row_i}"
+                    position = self.positions[name]
+                    print(name)
+
+                    self._make_text_and_box(name, col_i, position, previous_height)
+                    previous_height -= self.dimensions[name][1]
+
+                except KeyError:
+                    pass
+
+    def _make_text_and_box(self, name, col_i, position, previous_height):
         # Set the width, which is the x position, relative to other columns of this row
         width = sum([self.widths[str(i - 1)] for i in range(col_i + 1)])
         if col_i != 0:
@@ -86,9 +89,9 @@ class PrismaPlot:
         x_d, y_d, _ = obj.dimensions / 2
         obj.select_set(False)
 
-        self.make_bounding_box(x, y, x_d, y_d, name, obj)
+        self._make_bounding_box(x, y, x_d, y_d, name, obj)
 
-    def make_bounding_box(self, x, y, x_d, y_d, name, obj):
+    def _make_bounding_box(self, x, y, x_d, y_d, name, obj):
         # Create the bounding box
         vert_list = [((x - x_d) - self.line_width, (y + y_d) + self.line_width, -0.1),
                      ((x - x_d) - self.line_width, (y - y_d) - self.line_width, -0.1),
@@ -137,9 +140,9 @@ class PrismaPlot:
         for i, v in enumerate(cords):
             if i > 0:
                 vert_list.append([(0 - (self.line_width / 2), max(cords[i - 1]), -self.line_width),
-                              (0 - (self.line_width / 2), min(v), -self.line_width),
-                              (0 + (self.line_width / 2), min(v), -self.line_width),
-                              (0 + (self.line_width / 2), max(cords[i - 1]), -self.line_width)])
+                                  (0 - (self.line_width / 2), min(v), -self.line_width),
+                                  (0 + (self.line_width / 2), min(v), -self.line_width),
+                                  (0 + (self.line_width / 2), max(cords[i - 1]), -self.line_width)])
 
         # Create the center line object, set the origin to the location of the temp object then delete it
         face_list = chunk_list([i for i in range(len(vert_list) * 4)], len(vert_list))
@@ -147,6 +150,28 @@ class PrismaPlot:
         mesh.from_pydata(flatten(vert_list), [], face_list)
         box_obj.location = temp_obj.location
         bpy.ops.object.delete()
+
+    def _create_links(self):
+
+        # Select the second column
+        join_lines = [box for box in self.box_names if box.split("-")[0] == "1"]
+
+        # For each require line
+        for line in join_lines:
+
+            # Create the selection, and set them mid point y to be the locations y.
+            # Set x bound to be location x - min of local x from center
+            obj = bpy.data.objects[line]
+            line_y = obj.location[1]
+            line_x = obj.location[0] + min([v.co[0] for v in obj.data.vertices])
+
+            # Create the horizontal lines for this required link
+            vert_list = [(0, line_y + (self.line_width / 2), -self.line_width),
+                         (0, line_y - (self.line_width / 2), -self.line_width),
+                         (line_x, line_y - (self.line_width / 2), -self.line_width),
+                         (line_x, line_y + (self.line_width / 2), -self.line_width)]
+            box_obj, mesh = make_mesh(f"{obj.name}_link", self.box_colour)
+            mesh.from_pydata(vert_list, [], [[0, 1, 2, 3]])
 
 
 PrismaPlot()
