40d0d16fe88547612ce62e57e423bc5ba102ea51
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-11-10 16:43:14 +0000

    With scaling

--- pyBlendFigures/BlendFiles/Histogram.py
+++ pyBlendFigures/BlendFiles/Histogram.py
@@ -2,21 +2,33 @@ from blendSupports.Meshs.mesh_ref import make_mesh
 from csvObject import CsvObject
 import bpy
 
+file_path = r"C:\Users\Samuel\PycharmProjects\ScarletExposure\Figures and Tables\SummaryCsvs\S.csv"
+name_index = 0
 isolate = 1
 
-for i, row in enumerate(CsvObject(r"I:\Work\DataBases\Disease Notifications\ErrorLog\ErrorByFileEdit.csv").row_data):
+
+# TODO: Scaling directly is grim. We need to just standardise the values to be between the min and max by diving all by the max value
+y_scale = 0.0025
+
+
+
+for i, row in enumerate(CsvObject(file_path).row_data):
     print(i)
 
-    obj, mesh = make_mesh(row[0], (0.1, 0.1, 0.1, 0.1))
+    obj, mesh = make_mesh(row[name_index], (0.1, 0.1, 0.1, 0.1))
 
     verts = [
-        (i, int(row[isolate]), 0),
+        (i, float(row[isolate]), 0),
         (i, 0, 0),
         (i + 1, 0, 0),
-        (i + 1, int(row[isolate]), 0)
+        (i + 1, float(row[isolate]), 0)
     ]
 
     mesh.from_pydata(verts, [], [[0, 1, 2, 3]])
-    bpy.ops.object.select_all(action='DESELECT')
+    obj.select_set(True)
 
-bpy.ops.wm.save_as_mainfile(filepath=r"I:\Work\DataBases\Disease Notifications\ErrorLog\ErrorBlends\Error1.blend")
+    bpy.ops.transform.resize(value=(1, y_scale, 1))
+
+    print(obj.name)
+
+    bpy.ops.object.select_all(action='DESELECT')
