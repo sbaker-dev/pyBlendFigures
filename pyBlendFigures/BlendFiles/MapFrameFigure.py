46f57758d98874d5e72ce86d159cf6effd3194f3
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-11-15 16:58:13 +0000

    Just change the node colour rather than add a new node

--- pyBlendFigures/BlendFiles/MapFrameFigure.py
+++ pyBlendFigures/BlendFiles/MapFrameFigure.py
@@ -1,30 +1,27 @@
-from blendSupports.Nodes.emission_node import create_emission_node
 from miscSupports import load_json
+from pathlib import Path
 import bpy
 
 frame_dict = load_json(r"C:\Users\Samuel\PycharmProjects\pyBlendFigures\TestV2\Map2\Test2\UE_Values.txt")
 
+write_directory = r"I:\Work\Figures_and_tables\Depreivation indexes\UEOverTime"
+
 for frame_id, frame_place_values in frame_dict.items():
 
     for index, (place, colour) in enumerate(frame_place_values.items()):
-        print(f"{index}/{len(frame_place_values)}")
+        print(f"F{frame_id}: {index}/{len(frame_place_values)}")
 
+        # Deselect any objects
         bpy.ops.object.select_all(action='DESELECT')
 
+        # Isolate the current object
         obj = bpy.context.scene.objects.get(place)
         obj.select_set(True)
-        create_emission_node(obj, colour, 1, f'F{frame_id}-{place}')
 
-        # Isolate the index of that material, set it to active
-        material_index = {mat.name: i for i, mat in enumerate(obj.data.materials)}[f'F{frame_id}-{place}']
-        bpy.context.object.active_material_index = material_index
+        # Isolate the first material emission node, change its colour to the colour required
+        mat = obj.data.materials[0]
+        emission = mat.node_tree.nodes.get('Emission')
+        emission.inputs[0].default_value = colour
 
-        # Assign this material
-        bpy.ops.object.editmode_toggle()
-        bpy.ops.mesh.select_all(action='SELECT')
-        bpy.ops.object.material_slot_assign()
-        bpy.ops.mesh.select_all(action='DESELECT')
-        bpy.ops.object.editmode_toggle()
-        break
-
-    break
+    bpy.context.scene.render.filepath = str(Path(write_directory, f"{frame_id}.png").absolute())
+    bpy.ops.render.render(write_still=True)
