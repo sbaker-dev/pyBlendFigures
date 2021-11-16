2307f0cdfddfc80ae4cbe163c1948d5863cd81b1
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-11-16 14:21:10 +0000

    Save y within list rather than return

--- pyBlendFigures/BlendFiles/Scatter.py
+++ pyBlendFigures/BlendFiles/Scatter.py
@@ -5,6 +5,7 @@ from blendSupports.Meshs.text import make_text
 
 from miscSupports import load_json
 from csvObject import CsvObject
+import math
 import sys
 import bpy
 
@@ -19,15 +20,19 @@ class Scatter:
         self.y_index = 2
         self.ico_scale = 0.5
 
+        self._y_max = []
+
         self._make_point_groups()
+
+        print(math.ceil(max(self._y_max)))
 
     def _make_point_groups(self):
         [self.make_group(group, points) for group, points in self.scatter_groups.items()]
 
     def make_group(self, group, points):
-        obj, x_mid, y_max = self.make_vertex_holder(group, points)
+        obj, x_mid = self.make_vertex_holder(group, points)
 
-        if (x_mid != 0) and (y_max != 0):
+        if x_mid != 0:
             make_text(group, x_mid, -6, group, 5, (0.25, 0.25, 0.25, 1), 'CENTER')
 
         self.link_ico(obj)
@@ -41,9 +46,11 @@ class Scatter:
         mesh.from_pydata(vertexes, [], [])
 
         if len(vertexes) > 0:
-            return obj, (min(x_points) + max(x_points)) / 2, max([p[self.y_index] for p in points])
+            self._y_max.append(max([p[self.y_index] for p in points]))
+            return obj, (min(x_points) + max(x_points)) / 2,
         else:
-            return obj, 0, 0
+            self._y_max.append(0)
+            return obj, 0
 
     def link_ico(self, obj):
         bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=1, radius=1, enter_editmode=False, align='WORLD',
